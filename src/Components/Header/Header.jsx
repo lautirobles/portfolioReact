1005a4b87a680360677398501159904b3edd194e
Lautaro <latirobles@gmail.com>
2024-07-29 21:31:11 -0300

    Estructura de proyecto React creada

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,21 @@
+
+
+const Header = () =>{
+
+
+    return(
+        <header>
+            <img className="logo" src="./../../../public/logo2.png" alt="Logo"/>
+            <nav>
+                <a href="#inicio" class="centro">Inicio</a>
+                <a href="#servicios" class="centro">Servicios</a>
+                <a href="#acerca" class="centro">Sobre mi</a>
+                <a href="#proyectos" class="centro">Proyectos</a>
+                <a href="#contacto" class="centro">Contacto</a>
+                <a href="" class="derecha">Contratame</a>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
