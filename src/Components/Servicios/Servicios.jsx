108a6a9b94b1d9e060eb5ba2f825c94b6fbae7e3
Lautaro <latirobles@gmail.com>
2024-07-30 19:00:05 -0300

    Personalizacion de Servicios realizada

--- src/Components/Servicios/Servicios.jsx
+++ src/Components/Servicios/Servicios.jsx
@@ -1,30 +1,125 @@
 import * as Icon from 'react-bootstrap-icons'
+import { Flex, Box, Text } from '@chakra-ui/react'
 
 const Servicios = () =>{
 
-
     return(
-        <section id="servicios" className="servicios">
-            <h2>Servicios</h2>
-            <p>Actualmente, estos son los servicios en los que trabajo en cuanto a desarrollo de software.</p>
-            <div className="cajas-servicios">
-                <div className="serv">
-                    <Icon.FiletypePy width='2.5em' height='2.5em'/>
-                    <h3>Programas Python</h3>
-                    <p>Realizo programas desarrollados con python.</p>
-                </div>    
-                <div className="serv">
-                    <Icon.BrowserChrome width='2.5em' height='2.5em'/>
-                    <h3>Diseño de páginas</h3>
-                    <p>Realizo páginas web responsive a pedido con previa reunión para acordar los detalles de la misma </p>
-                </div>
-                <div className="serv">
-                    <Icon.Cpu width='2.5em' height='2.5em'/>
-                    <h3>Programas con Arduino</h3>
-                    <p>Realizo programas para microcontroladores a través de arduino o mikroC, utilizando el lenguaje C++.</p>
-                </div>
-            </div>    
-        </section>
+        <Flex 
+        as='section' 
+        id="servicios"
+        flexWrap='wrap'
+        justifyContent='center'
+        textAlign='center'
+        ml='7.5vw'
+        mr='7.5vw'
+        mt='10em'
+        mb='7em'
+        >
+            <Text 
+            as='h2'
+            fontSize='2.5em'
+            width='100vw'
+            fontWeight='semibold'
+            >Servicios</Text>
+            <Text 
+            as='p'
+            color='#ACA4B4'
+            fontSize='1.2em'
+            >Actualmente, estos son los servicios en los que trabajo en cuanto a desarrollo de software.</Text>
+            <Flex 
+            as='div'
+            width='100%'
+            justifyContent='space-between'
+            >
+                <Box 
+                as='div'
+                flex='1'
+                p='4em 0'
+                m='2em'
+                borderRadius='15px'
+                transition='.5s background-color, .5s box-shadow, .5s color'
+                _hover={{
+                    backgroundColor: '#5d17bf',
+                    boxShadow: '0 0 .5em black',
+                    color: '#fcfcfc'
+                }} 
+                >
+                    <Box 
+                    display='flex'
+                    justifyContent='center'
+                    ><Icon.FiletypePy width='2.5em' height='2.5em'/></Box>
+                    <Text 
+                    as='h3'
+                    m='1.5em 0 0'
+                    fontSize='1.5em'
+
+                    >Programas Python</Text>
+                    <Text 
+                    as='p'
+                    color='#ACA4B4'
+                    fontSize='1.2em'
+                    >Realizo programas desarrollados con python.</Text>
+                </Box>
+
+                <Box 
+                as='div'
+                flex='1'
+                p='4em 0'
+                m='2em'
+                borderRadius='15px'
+                transition='.5s background-color, .5s box-shadow, .5s color'
+                _hover={{
+                    backgroundColor: '#5d17bf',
+                    boxShadow: '0 0 .5em black',
+                    color: '#fcfcfc'
+                }} 
+                >
+                    <Box 
+                    display='flex'
+                    justifyContent='center'
+                    ><Icon.BrowserChrome width='2.5em' height='2.5em'/></Box>
+                    <Text 
+                    as='h3'
+                    m='1.5em 0 0'
+                    fontSize='1.5em'
+                    >Diseño de páginas</Text>
+                    <Text 
+                    as='p'
+                    color='#ACA4B4'
+                    fontSize='1.2em'
+                    >Realizo páginas web responsive a pedido con previa reunión para acordar los detalles de la misma</Text>
+                </Box>    
+                
+                <Box 
+                as='div'
+                flex='1'
+                p='4em 0'
+                m='2em'
+                borderRadius='15px'
+                transition='.5s background-color, .5s box-shadow, .5s color'
+                _hover={{
+                    backgroundColor: '#5d17bf',
+                    boxShadow: '0 0 .5em black',
+                    color: '#fcfcfc'
+                }} 
+                >
+                    <Box
+                    display='flex'
+                    justifyContent='center'
+                    ><Icon.Cpu width='2.5em' height='2.5em'/></Box>
+                    <Text 
+                    as='h3'
+                    m='1.5em 0 0'
+                    fontSize='1.5em'
+                    >Programas con Arduino</Text>
+                    <Text 
+                    as='p'
+                    color='#ACA4B4'
+                    fontSize='1.2em'
+                    >Realizo programas para microcontroladores a través de arduino o mikroC, utilizando el lenguaje C++.</Text>
+                </Box> 
+            </Flex>    
+        </Flex>
     )
 }
 
